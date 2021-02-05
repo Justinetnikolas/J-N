ab684e6d6157caa94fd89f85906811dc8a29c176
Justinetnikolas <78555035+Justinetnikolas@users.noreply.github.com>
2021-02-05 11:41:01 -0500

    Add files via upload

---
+++ JustinScript.js
@@ -0,0 +1,10 @@
+function showMessage(){
+document.getElementById("messageSecret").innerHTML="https://forms.gle/rKfGhpGHiztyFYtT7"; 
+}
+
+ var clicks=0;
+function countClicks(){
+   
+    clicks +=1;
+    document.getElementById("nombreDeCliques").innerHTML=clicks;
+}
